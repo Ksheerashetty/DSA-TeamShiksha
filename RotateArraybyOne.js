a3c0d00fd3d5fa2a4dce457b3c7f697a31cb1007
ksheerashetty <65451950+Ksheerashetty@users.noreply.github.com>
2026-01-13 00:23:27 +0530

    Add function to rotate array by one position
    Problem Link: https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1

---
+++ RotateArraybyOne.js
@@ -0,0 +1,30 @@
+// Link: https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+// Rotate Array by One
+// Difficulty: Basic Accuracy: 69.6% Submissions: 350K+ Points: 1 Average Time: 20m
+// Given an array arr, rotate the array by one position in clockwise direction.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4, 5]
+// Output: [5, 1, 2, 3, 4]
+// Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+// Input: arr[] = [9, 8, 7, 6, 4, 2, 1, 3]
+// Output: [3, 9, 8, 7, 6, 4, 2, 1]
+// Explanation: After rotating clock-wise 3 comes in first position.
+// Constraints:
+// 1<=arr.size()<=105
+// 0<=arr[i]<=105
+
+class Solution {
+    rotate(arr) {
+         let n=arr.length,temp=arr[n-1]
+        for(let i=n-1;i>0;i--){
+            arr[i]=arr[i-1]
+        }
+        arr[0]=temp
+        return arr
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(1)
