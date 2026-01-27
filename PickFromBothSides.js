152628176a453a6a916c7eabbc016c27d88387a6
ksheerashetty <65451950+Ksheerashetty@users.noreply.github.com>
2026-01-27 23:58:30 +0530

    Create PickFromBothSides.js
    Problem Link: https://www.interviewbit.com/problems/pick-from-both-sides/

---
+++ PickFromBothSides.js
@@ -0,0 +1,69 @@
+// Pick from both sides
+// Problem Link: https://www.interviewbit.com/problems/pick-from-both-sides/
+// Problem Description
+
+// Given an integer array A of size N.
+// You have to pick exactly B elements from either left or right end of the array A to get the maximum sum.
+// Find and return this maximum possible sum.
+
+// NOTE: Suppose B = 4 and array A contains 10 elements then
+
+// You can pick the first four elements or can pick the last four elements or can pick 1 from the front and 3 from the back etc. you need to return the maximum possible sum of elements you can pick.
+
+
+// Problem Constraints
+// 1 <= N <= 105
+// 1 <= B <= N
+// -103 <= A[i] <= 103
+
+// Input Format
+// First argument is an integer array A.
+// Second argument is an integer B.
+
+// Output Format
+// Return an integer denoting the maximum possible sum of elements you picked.
+
+// Example Input
+// Input 1:
+//  A = [5, -2, 3 , 1, 2]
+//  B = 3
+
+// Input 2:
+//  A = [1, 2]
+//  B = 1
+
+// Example Output
+// Output 1:
+//  8
+// Output 2:
+//  2
+
+// Example Explanation
+// Explanation 1:
+//  Pick element 5 from front and element (1, 2) from back so we get 5 + 1 + 2 = 8
+// Explanation 2:
+//  Pick element 2 from end as this is the maximum we can get 2
+
+1.Brute Force
+module.exports = {
+	//param A : array of integers
+	//param B : integer
+	//return an integer
+	solve: function (A, B) {
+		let res = -Infinity
+		for (let i = 0; i <= B; i++) {
+			let sum = 0
+			for (let j = 0; j < i; j++) {
+				sum += A[j]
+			}
+			for (let k = 0; k < B - i; k++) {
+				sum += A[A.length - 1 - k]
+			}
+			res = Math.max(res, sum)
+		}
+		return res;
+	}
+};
+
+Time Complexity: O(n)
+Space Complexity: O(1)
